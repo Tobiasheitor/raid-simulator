5f3232e973460debee7896b1d075e3e2352ac87e
Tobias Heitor <tobias.goettert_est@compasso.com.br>
2019-06-11 13:27:44 -0300

    Criacao de metodo para encontrar diretorios a partir da raiz

--- menu.java
+++ menu.java
@@ -1,23 +1,36 @@
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.nio.file.*;
 
 public class menu {
 
     public static void main(String[] args) {
-        Path p1 = Paths.get("C:\\Users\\tobias.goettert_est\\Desktop\\java teste\\");
-        System.out.println("Path created: " + p1.toString());
-        System.out.println("Path parent: " + p1.getParent());
-
-        System.out.println(isValidPath(p1));
+        getAllDirectories().stream().forEach(x->System.out.println(x.getFileName()));;
     }
 
     private static boolean isValidPath(Path path) {
+        if (!Files.exists(path)) {
+            return false;
+        }
         try {
             path.toRealPath();
         } catch (IOException e) {
             return false;
         }
         return true;
+    }
+
+    private static ArrayList<Path> getAllDirectories() {
+        ArrayList<Path> caminhos = new ArrayList<>();
+        String pwd = System.getProperty("user.dir");
+        Path dir = Paths.get(pwd);
+        try {
+            Files.walk(dir, 1).forEach(path -> caminhos.add(path));
+        } catch (IOException e) {
+            System.out.println("Error " + e);
+        }
+        return caminhos;
     }
 }
