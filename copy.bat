f44186897cbdccfc590f27da7b760c7f512585f3
Tobias Heitor <tobias.goettert_est@compasso.com.br>
2019-06-14 17:15:33 -0300

    Versao final

--- copy.bat
+++ copy.bat
@@ -6,8 +6,12 @@ if not exist %source% echo Invalid directory.
 
 if not exist %destination% mkdir %destination%
 
-xcopy %source%* %destination% /s/y/c/v/r
+:while
+rm -r %destination%
+xcopy %source%* %destination% /s/y/c/v
 echo. 
 echo Finished copying %source% to %destination%
 echo.
 echo Created %destination%
+sleep 5
+goto :while
